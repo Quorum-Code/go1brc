9c21c8b8c088e33ec84b384ed564c704330e3ea1
Evan Putnam <quorumcode@gmail.com>
2024-02-28 15:17:54 -0800

    Simple data generator prototype

---
+++ measurement-data/measurement-builder.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"os"
+)
+
+func main() {
+	cities := []string{"Sacramento", "Arcata", "Eureka", "Trinidad", "San Fransisco", "Oakland", "Monterey"}
+
+	fmt.Println("Creating file...")
+	f, err := os.Create("./measurements.txt")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Creating entries...")
+	for i := 0; i < 10; i++ {
+		// get random city
+		city := cities[rand.IntN(len(cities))]
+
+		// get random val
+		temp := rand.Float64()*200 - 99.9
+
+		_, err := f.WriteString(fmt.Sprintf("%v:%.1f\n", city, temp))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	fmt.Println("Finished creating measurements.txt...")
+}
